79eb9c8adaa55606e9b1f631aeacc5e69c6b6a13
bklynate <nathanielcfa@gmail.com>
2015-05-26 03:05:42 -0400

    Working with 'hit' and 'stay' ability

--- main.rb
+++ main.rb
@@ -6,6 +6,33 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => '19874310BKLYN'
 
+helpers do
+  def hand_value(cards)
+    arr = cards.map{ |value| value[1] }
+    total = 0
+
+    arr.each do |value|
+      if value == 'A'
+        total += 11
+      elsif value.to_i == 0
+        total += 10
+      else
+        total += value
+      end
+    end
+
+    arr.select { |value| value == 'A'}.count.times do
+      total -= 10 if total > 21
+    end
+
+    total
+  end
+end
+
+before do
+  @show_hit_and_stay = true
+end
+
 get '/' do
   if session[:username]
     redirect '/game'
@@ -23,9 +50,18 @@ post '/form' do
   redirect '/game'
 end
 
-post '/hit' do
-  new_card = session[:deck].pop 
-  session[:player_cards] << new_card
+post '/hit' do 
+  session[:player_cards] << session[:deck].pop
+  if hand_value(session[:player_cards]) > 21
+    @error = "Looks like you have busted."
+    @show_hit_and_stay = false
+  end
+  erb :game
+end
+
+post '/stay' do
+  @success = 'You have chosen to stay.'
+  @show_hit_and_stay = false
   erb :game
 end
 
